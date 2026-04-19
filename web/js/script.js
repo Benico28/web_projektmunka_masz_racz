fd04c610c60d7daac0291dbbb4de5c7f065d4c38
Raczika08 <167176087+Raczika08@users.noreply.github.com>
2026-04-19 23:10:50 +0200

    web

--- web/js/script.js
+++ web/js/script.js
@@ -1,5 +1,7 @@
 var tema = document.querySelector(".tema");
 var body = document.body;
+var gombF = document.querySelector(".gombF")
+var gombV = document.querySelector(".gombV")
 
 tema.addEventListener('click', () => {
     body.classList.toggle('light-mode');
@@ -8,9 +10,17 @@ tema.addEventListener('click', () => {
         tema.textContent = '🌙';
         tema.classList.remove("btn-outline-warning");
         tema.classList.add("btn-outline-danger");
+        gombF.classList.remove("btn-outline-warning");
+        gombF.classList.add("btn-outline-danger");
+        gombV.classList.remove("btn-outline-warning");
+        gombV.classList.add("btn-outline-danger");
     } else {
         tema.textContent = '☀️';
         tema.classList.remove("btn-outline-danger");
         tema.classList.add("btn-outline-warning");
+        gombF.classList.remove("btn-outline-danger");
+        gombF.classList.add("btn-outline-warning");
+        gombV.classList.remove("btn-outline-danger");
+        gombV.classList.add("btn-outline-warning");
     }
 });
