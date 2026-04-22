8d8c2732a697e9e39690e3259c73c9232ee4439f
Raczika08 <167176087+Raczika08@users.noreply.github.com>
2026-04-22 13:26:41 +0200

    web

--- web/js/script.js
+++ web/js/script.js
@@ -3,12 +3,14 @@ var temaKicsi = document.querySelector(".temaKicsi");
 var body = document.body;
 var gombF = document.querySelector(".gombF")
 var gombV = document.querySelector(".gombV")
+var logo = document.querySelector(".logo")
 
 tema.addEventListener('click', () => {
     body.classList.toggle('light-mode');
     
     if (body.classList.contains('light-mode')) {
         tema.textContent = '🌙';
+        logo.innerHTML = '<img class="w-50" src="kepek/logoVilagos.png" alt="logo" title="logo"></img>'
         tema.classList.remove("btn-outline-warning");
         tema.classList.add("btn-outline-danger");
         gombF.classList.remove("btn-outline-warning");
@@ -17,6 +19,7 @@ tema.addEventListener('click', () => {
         gombV.classList.add("btn-outline-danger");
     } else {
         tema.textContent = '☀️';
+        logo.innerHTML = '<img class="w-50" src="kepek/logo.png" alt="logo" title="logo">'
         tema.classList.remove("btn-outline-danger");
         tema.classList.add("btn-outline-warning");
         gombF.classList.remove("btn-outline-danger");
@@ -30,6 +33,7 @@ temaKicsi.addEventListener('click', () => {
     
     if (body.classList.contains('light-mode')) {
         temaKicsi.textContent = '🌙';
+        logo.innerHTML ='<img class="w-50" src="kepek/logoVilagos.png" alt="logo" title="logo">'
         temaKicsi.classList.remove("btn-outline-warning");
         temaKicsi.classList.add("btn-outline-danger");
         gombF.classList.remove("btn-outline-warning");
@@ -38,6 +42,7 @@ temaKicsi.addEventListener('click', () => {
         gombV.classList.add("btn-outline-danger");
     } else {
         temaKicsi.textContent = '☀️';
+        logo.innerHTML = '<img class="w-50" src="kepek/logo.png" alt="logo" title="logo">'
         temaKicsi.classList.remove("btn-outline-danger");
         temaKicsi.classList.add("btn-outline-warning");
         gombF.classList.remove("btn-outline-danger");
